98d09075e06a9893c0e418338ca7de74cb3fe750
Antoine Tavant <antoinetavant@hotmail.fr>
2024-11-20 15:23:31 +0100

    create hatch env and rename the project

--- src/energy_forecast_django/energy_forecast_django/urls.py
+++ src/energy_forecast_django/energy_forecast_django/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for energy_forcast_django project.
+URL configuration for energy_forecast_django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- src/energy_forecast_django/energy_forecast_django/wsgi.py
+++ src/energy_forecast_django/energy_forecast_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for energy_forcast_django project.
+WSGI config for energy_forecast_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energy_forcast_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energy_forecast_django.settings')
 
 application = get_wsgi_application()
